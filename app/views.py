3d21c8220a9611c756ed630a87108aabc25dcde1
Oktay Sabak <oktaysabak@yandex.com>
2018-06-23 04:53:41 +0300

    first level of my flask app

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/home')
+def index():
+    user = 'Guest'
+    return render_template('index.html', title='H0me Page', user = user)
+@app.route('/users')
+def users():
+    userList = [
+        {'name':'Oktay', 'title':'Bonboşş Adam'},
+        {'name':'Doğuş', 'title':'Daha Bonboşş Adam'},
+    ]
+    return render_template('users.html', title = 'Users Page', userList= userList)
