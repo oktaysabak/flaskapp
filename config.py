a06cca64513c7aff89a546568f805d7da628fd89
Oktay Sabak <oktaysabak@yandex.com>
2018-07-15 02:46:05 +0300

    database operations

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'deneme-anahtar-gizli'
+
+    #db
+    path = 'sqlite:///' + os.path.join(BASEDIR,'database.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or path
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
